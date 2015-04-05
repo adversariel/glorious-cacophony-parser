8fa6f266ffae85fbe71756aadaece36f06861abc
neurophoenix <ariel.herbertvoss@gmail.com>
2015-04-05 16:25:57 -0600

    Implemented position-to-frequency note conversion. Added unit tests.

---
+++ Cacophony/Cacophony/Noises.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cacophony
+{
+    public class Noises
+    {
+        static void Main(string[] args)
+        {
+            int note = PositionToFreq(40);
+            Console.Beep(note, 500);
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        public static int PositionToFreq(int pos)
+        {
+            return (int) Math.Floor(Math.Pow(2, (float)(pos - 49) / 12) * 440);
+        }
+    }
+}
---
+++ Cacophony/Testing/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cacophony;
+
+namespace Testing
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestPosToFreq()
+        {
+            int c = Noises.PositionToFreq(40);
+
+            Assert.AreEqual(261, c);
+        }
+    }
+}
